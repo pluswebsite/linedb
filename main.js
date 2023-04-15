91c141ade8b5fbbc56a55d116ae79169df60d636
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-04-15 15:33:36 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+class LineDB(){
+  constructor(api-key, bin-id){
+    this.bin = bin-id;
+    this.api = api-key;
+    this.data;
+  }
+  get(callback){
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        callback(req.responseText);
+        this.data = req.responseText;
+      }
+    };
+
+     req.open("GET", "https://api.jsonbin.io/v3/b/" + this.bin + "/?meta=false", true);
+     req.setRequestHeader("X-Master-Key", this.api);
+     req.send();
+  }
+  uptade(key, value, callback=console.log){
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        callback(req.responseText);
+      }
+    };
+    
+    this.data[key] = value;
+
+    req.open("PUT", "https://api.jsonbin.io/v3/b/" + this.bin, true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("X-Master-Key", this.api);
+    req.send(this.data);
+  }
+}
